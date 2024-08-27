ee4406bdd83d57d36329428dbdb70719e0b9b9a2
Tycee02 <wakilaminat@gmail.com>
2024-08-27 18:43:49 +0100

    py file

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,19 +2,21 @@
 """
 Get locale from request
 """
-from flask import Flask, render_template, request
 from flask_babel import Babel
-
-app = Flask(__name__)
+from flask import Flask, render_template, request
 
 
 class Config:
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
